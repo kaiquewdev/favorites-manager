d1448a7f9af60c055a221512798c86c4ecb93684
Kaique Silva <kaiquesilva@visie.com.br>
2011-08-27 21:52:02 -0300

    Projeto inciado

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'My New App'
+settings.subtitle = 'powered by web2py'
+settings.author = 'you'
+settings.author_email = 'you@example.com'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '717ac61d-d1dc-470b-b2ee-8da8e1059ef2'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
