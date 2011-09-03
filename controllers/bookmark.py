a4d4516c71abbc9460ff8fea04ba730cbcc71c1d
Kaique Silva <kaiquesilva@visie.com.br>
2011-09-03 01:02:39 -0300

    Busca -Procurando somente pelo nome do bookmark

--- controllers/bookmark.py
+++ controllers/bookmark.py
@@ -4,62 +4,73 @@ Controller para bookmarks onde terao todas as funcoes para trabalhar
 com os marcadores
 '''
 def index():
-	'''
-	Inicio dos bookmarks
-	'''
-	redirect(URL('new'))
+		'''
+		Inicio dos bookmarks
+		'''
+		redirect(URL('new'))
 
 @auth.requires_login()	
 def new():
-	'''
-	Cria um novo bookmark
-	'''
-	form = SQLFORM(db.bookmarks, fields=['name','url','note','keywords'])
-    
-	if form.accepts(request.vars, session, dbio=False):
-		db(db.bookmarks).update(user = auth.user.email, creation = request.now)
-		response.flash = T('New bookmark was created !')
-	elif form.errors:
-		response.flash = T('Oops, look who\'s wrong !')
-
-	return {'form':form}
+		'''
+		Cria um novo bookmark
+		'''
+		form = SQLFORM(db.bookmarks, fields=['name','url','note','keywords'])
+	    
+		if form.accepts(request.vars, session, dbio=False):
+			db(db.bookmarks).update(user = auth.user.email, creation = request.now)
+			response.flash = T('New bookmark was created !')
+		elif form.errors:
+			response.flash = T('Oops, look who\'s wrong !')
+	
+		return {'form':form}
 	
 @auth.requires_login()
 def show():
-	'''
-	Mostra os bookmarks
-	'''
-	mymarks = db().select(db.bookmarks.ALL, orderby=db.bookmarks.creation)
-	return {'marks':mymarks}
+		'''
+		Mostra os bookmarks
+		'''
+		mymarks = db().select(db.bookmarks.ALL, orderby=db.bookmarks.creation)
+		return {'marks':mymarks}
 
 @auth.requires_login()
 def remove():
-	'''
-	Remove um bookmark
-	'''
-	if request.args(0):
-		db(db.bookmarks.id == request.args(0)).delete()
-		redirect(URL('bookmark','show'));
-	else:
-		redirect(URL('bookmark','show'))
+		'''
+		Remove um bookmark
+		'''
+		if request.args(0):
+			db(db.bookmarks.id == request.args(0)).delete()
+			redirect(URL('bookmark','show'));
+		else:
+			redirect(URL('bookmark','show'))
 		
 @auth.requires_login()
 def edit():
-	'''
-	Edita um bookmark existente
-	'''
-	rec = db.bookmarks(request.args(0)) or redirect(URL('show'))
-	form = SQLFORM(db.bookmarks, rec, fields=['name','url'])
+		'''
+		Edita um bookmark existente
+		'''
+		rec = db.bookmarks(request.args(0)) or redirect(URL('show'))
+		form = SQLFORM(db.bookmarks, rec, fields=['name','url'])
+		
+		if form.accepts(request.vars, session):
+			redirect(URL('bookmark','show'))
+		elif form.errors:
+			response.flash = T('Oops, look whos wrong !')
 	
-	if form.accepts(request.vars, session):
-		redirect(URL('bookmark','show'))
-	elif form.errors:
-		response.flash = T('Oops, look whos wrong !')
-
-	return {'form':form}
+		return {'form':form}
 
 def search():
-	'''
-	Faz a procura por bookmarks
-	'''
+		'''
+		Faz a procura por bookmarks
+		'''
+		if request.vars.search == '' or request.vars.search == None:
+			redirect(URL('default','index'))
+		else:
+			f = db(db.bookmarks.id>0).select()
+			f = f.find(lambda s:request.vars.search in s.name and s.status == 'public').sort(lambda s:s.name) or None
+			
+			if f == None:
+				response.flash = T('Try again !')
+			
+		
+		return {'search':f}
 
--- controllers/default.py
+++ controllers/default.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 ### required - do no delete
 def user(): return dict(form=auth())
-def download(): return response.download(request,db)
+def download(): return response.download(request, db)
 def call():
-    session.forget()
-    return service()
+        session.forget()
+        return service()
 ### end requires
 
 def index():
-	bookmarks = db(db.bookmarks.status == 'public').select()
-	search = FORM(T('Search:'), INPUT(_name='search',_type='text'), INPUT(_value='Find', _type='submit'))
-	
-	return {'marks':bookmarks, 'search':search}
+    	bookmarks = db(db.bookmarks.status == 'public').select()
+    	search = FORM(T('Search:'), INPUT(_name='search', _type='text', requires=IS_NOT_EMPTY()), INPUT(_value=T('Go'), _type='submit'), _action=URL('bookmark', 'search'), _method='post')
+        session.search = search         
+    	return {'marks':bookmarks, 'search':search}
 
 def about():return {}
 def error():return {}
