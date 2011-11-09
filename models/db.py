e7fa35949d05944df83161abe6076ecd2a459506
Kaique Silva <kaiquesilva@visie.com.br>
2011-11-09 00:24:09 -0200

    db ok

--- models/db.py
+++ models/db.py
@@ -14,8 +14,8 @@ if request.env.web2py_runtime_gae:            # if running on Google App Engine
     # from google.appengine.api.memcache import Client
     # session.connect(request, response, db = MEMDB(Client()))
 else:
-    db = DAL('sqlite://storage.sqlite')       # else use a normal relational database
-    #db = DAL('mysql://root:dev@localhost/brands')      # if not, use SQLite or other DB
+    #db = DAL('sqlite://storage.sqlite')       # else use a normal relational database
+    db = DAL('mysql://brandmark:2wrBvmHlsD@brandmark.mysql.fluxflex.com/brandmark')      # if not, use SQLite or other DB
 
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
